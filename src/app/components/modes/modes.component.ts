a9db332a00c31a12edb26a0f6100edd4a224f270
leonid.kuzmychenko <leonid.kuzmychenko@ab-soft.net>
2021-02-22 01:36:53 +0200

    review

--- src/app/components/modes/modes.component.ts
+++ src/app/components/modes/modes.component.ts
@@ -14,10 +14,22 @@ export class ModesComponent implements OnInit {
   constructor(private http: HttpClient) {
     this.http.get<GameModeDto[]>('https://cocktails-making-training.herokuapp.com/modes')
       .subscribe((response) => {
-        // this.items = response.map((it) => it.gameModeNames.filter((it2) => it2.locale === 'EN')[0]).map((it) => it.name);
+        response.map((it) => {
+          it.gameModeNames.sort((name1, name2) => this.sortByName(name1.locale, name2.locale));
+        });
         this.items = response;
         console.log(response);
       });
+  }
+
+  sortByName(name1: string, name2: string): number {
+    if (name1 > name2) {
+      return 1;
+    }
+    if (name1 < name2) {
+      return -1;
+    }
+    return 0;
   }
 
   ngOnInit(): void {
