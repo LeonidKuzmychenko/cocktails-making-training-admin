5edb11f9fb7a9fbadcf3ae9c50230a762a2b340e
leonid.kuzmychenko <leonid.kuzmychenko@ab-soft.net>
2021-02-21 16:59:06 +0200

    spa

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {HomeComponent} from './home/home.component';
+import {CocktailsComponent} from './cocktails/cocktails.component';
+import {IngredientsComponent} from './ingredients/ingredients.component';
+import {ModesComponent} from './modes/modes.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'cocktails', component: CocktailsComponent},
+  {path: 'ingredients', component: IngredientsComponent},
+  {path: 'modes', component: ModesComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,36 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {HomeComponent} from './home/home.component';
+import {CocktailsComponent} from './cocktails/cocktails.component';
+import {IngredientsComponent} from './ingredients/ingredients.component';
+import {ModesComponent} from './modes/modes.component';
+
+
+// const routes: Routes = [
+//   {path: '', component: HomeComponent},
+//   {path: '/cocktails', component: CocktailsComponent},
+//   {path: '/ingredients', component: IngredientsComponent},
+//   {path: '/modes', component: ModesComponent}
+// ];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    CocktailsComponent,
+    IngredientsComponent,
+    ModesComponent
   ],
   imports: [
     BrowserModule,
+    // RouterModule.forRoot(routes),
     AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
